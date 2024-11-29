4dd2a0abd918cd4f408b9e45c6264116b88f3539
Emir Sen <emirsen@OrdinatledeEmir.lan>
2024-11-29 15:53:26 +0100

    JavaFxTest

--- JavaFXTest/JavaFXTest/src/main/java/fr/esiee/javafxtest/HelloController.java
+++ JavaFXTest/JavaFXTest/src/main/java/fr/esiee/javafxtest/HelloController.java
@@ -2,6 +2,7 @@ package fr.esiee.javafxtest;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Paint;
 
 // chaque cobtroller est relié à un fichier fxml
@@ -9,13 +10,24 @@ public class HelloController {
     @FXML
     private Label l_welcomeText;
     @FXML
-    protected void onHelloButtonClick() {
+    private TextField tf_inputName;
+
+    public void onHelloButtonClick() {
         //1- récupérer le inputName saisie par l'utilisateur dans tf_inputName
+        String inputName = tf_inputName.getText().trim();
         // Sie le nom est saisi :
-        //2- afficher dans le label l_welcomeText le message de bienvenu avec le inputName
+        if (!inputName.isEmpty()) {
+            //2- afficher dans le label l_welcomeText le message de bienvenu avec le inputName
+            l_welcomeText.setText("Bienvenue, " + inputName + "!");
+            l_welcomeText.setTextFill(Paint.valueOf("black")); // Couleur noire pour un message valide
+        }
         //sinon :
-        //3- changer la couleur du text du label l_welcomeText en rouge et afficher un text "Erreur.. saisi"
-        l_welcomeText.setText("Welcome to JavaFX Application!");
-        l_welcomeText.setTextFill(Paint.valueOf("red"));
+        else {
+
+            //3- changer la couleur du text du label l_welcomeText en rouge et afficher un text "Erreur.. saisi"
+            l_welcomeText.setText("Erreur : veuillez saisir un nom.");
+            l_welcomeText.setTextFill(Paint.valueOf("red"));
+        }
     }
 }
+
