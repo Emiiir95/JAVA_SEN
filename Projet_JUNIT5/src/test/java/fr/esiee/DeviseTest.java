88f7523815bae4a0356aa77b5858fac0b7f54002
Emir Sen <emirsen@OrdinatledeEmir.lan>
2025-01-24 12:58:43 +0100

    ETAPE 2 finis

--- Projet_JUNIT5/src/test/java/fr/esiee/DeviseTest.java
+++ Projet_JUNIT5/src/test/java/fr/esiee/DeviseTest.java
@@ -8,12 +8,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DeviseTest {
 
+    private static int compteurTest = 0;
+    private Devise m12CHF;
+    private Devise m14CHF;
+
     @BeforeEach
     void setUp() {
+        System.out.println("\nJe suis avant exécution d'une méthode de test");
+
+        compteurTest++;
+        System.out.println(compteurTest + "e passage avant exécution d'une méthode de test");
+
+        m12CHF= new Devise(12, "CHF");
+        m14CHF= new Devise(14, "CHF");
     }
 
     @AfterEach
     void tearDown() {
+        System.out.println(compteurTest + "e passage APRES exécution d'une méthode de test");
+        System.out.println("Je suis APRES exécution d'une méthode de test");
     }
 
     @Test
@@ -22,11 +35,18 @@ class DeviseTest {
 
     @Test
     void add() {
-        Devise m12CHF= new Devise(12, "CHF");
-        Devise m14CHF= new Devise(14, "CHF");
         Devise expected = new Devise(26, "CHF");
         Devise result = m12CHF.add(m14CHF);
 
         assertEquals(expected, result, "La somme des devises n'est pas correcte");
+    }
+
+    @Test
+    void equals() {
+        Devise m14USD = new Devise(14, "USD");
+
+        assertEquals(m12CHF, m12CHF, "m12CHF devrait être égal à lui-même");
+        assertNotEquals(m12CHF, m14CHF, "m12CHF ne devrait pas être égal à m14CHF");
+        assertNotEquals(m14CHF, m14USD, "m14CHF ne devrait pas être égal à m14USD");
     }
 }
